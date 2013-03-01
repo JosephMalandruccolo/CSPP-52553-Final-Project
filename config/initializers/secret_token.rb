e149fc0adebf925dc434e31452b135afc3400aaa
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-03-01 16:04:25 -0600

    Initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollegeLens::Application.config.secret_token = '26955d3592bfc8db6fa953c2d4bef1936ccd8dafc5fa0f5fb20597f71b668618911b304983f05ae3c20c8c51ead2ed0884bf84c656d54e7ff042de3bc6c9ea45'
