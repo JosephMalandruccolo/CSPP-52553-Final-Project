5337701e94bbba38182331a65a27b3d0a187cbe2
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-03-01 17:22:00 -0600

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :birthMonth, :birthYear, :birthday, :city, :email, :firstName, :gender, :lastName, :password_digest, :state
+  attr_accessible :birthMonth, :birthYear, :birthday, :city, :email, :firstName, :gender, :lastName, :password, :password_confirmation, :state
+
+  has_secure_password
 
   ##########################################
   # => CALLBACKS
@@ -7,10 +9,14 @@ class User < ActiveRecord::Base
   # ensures uniqueness of case insensitive emails
   before_save { |user| user.email = email.downcase }
 
+
   ##########################################
   # => VALIDATIONS
   ##########################################
   # form is complete
+  validates :password_confirmation, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :firstName, presence: true
   validates :lastName, presence: true
   validates :gender, presence: true
@@ -19,10 +25,7 @@ class User < ActiveRecord::Base
   validates :birthYear, presence: true
   validates :city, presence: true
   validates :state, presence: true
-
-  # form is correct
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  
 
 
 
