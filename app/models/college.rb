b4c7ec15bc3ea7809b140e3b07f46eaba5e76e95
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-03-01 19:53:00 -0600

    Add website column to College model

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,7 +1,11 @@
 class College < ActiveRecord::Base
   attr_accessible :city, :facebook, :instagram, :logo, :officialName, :shortName, :state, :twitter, :wikipedia
 
-  
 
-  
+  ##########################################
+  # => VALIDATIONS
+  ##########################################
+  validates :city, presence: true
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302014224) do
+ActiveRecord::Schema.define(:version => 20130302014743) do
 
   create_table "colleges", :force => true do |t|
     t.string   "officialName"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20130302014224) do
     t.string   "wikipedia"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "website"
   end
 
   create_table "users", :force => true do |t|
