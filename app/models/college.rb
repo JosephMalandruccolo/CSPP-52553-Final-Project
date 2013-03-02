b0f1bd56ed365abb031e2d4b5d6dea7804002d46
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-03-01 20:36:46 -0600

    Trivial updates to College new view

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,11 +1,16 @@
 class College < ActiveRecord::Base
-  attr_accessible :city, :facebook, :instagram, :logo, :officialName, :shortName, :state, :twitter, :wikipedia
+  attr_accessible :city, :facebook, :instagram, :logo, :officialName, :shortName, :state, :twitter, :wikipedia, :website
 
 
   ##########################################
   # => VALIDATIONS
   ##########################################
   validates :city, presence: true
+  validates :state, presence: true, length: { is: 2 }
+  validates :officialName, presence: true
+  validates :shortName, :presence => true
+  validates :logo, :presence => true
+  validates :website, presence: true
 
 
 end
