996a4656c7d7e3ffa5d173207b54623f5b33c682
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-03-04 17:02:03 -0600

    Update College show page

--- app/helpers/colleges_helper.rb
+++ app/helpers/colleges_helper.rb
@@ -1,2 +1,11 @@
 module CollegesHelper
+
+
+
+	DEFAULT_SEAL = '/assets/seal/black-chalkboard.jpg'
+	def show_seal(college)
+		image_tag(DEFAULT_SEAL, alt: college.shortName)
+	end
+
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
-	DEFAULT_URL = '/assets/200-default-avatar.png'
+	DEFAULT_AVATAR = '/assets/avatar/200-default-avatar.png'
 	def avatar_for(user)
-		image_tag(DEFAULT_URL, alt: user.name)
+		image_tag(DEFAULT_AVATAR, alt: user.name)
 	end
 end
