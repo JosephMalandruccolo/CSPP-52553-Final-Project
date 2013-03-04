fec085106d902c03df54c411c00c52bc3aa67861
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-03-04 16:15:25 -0600

    Fix bug in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   ##########################################
   # => VALIDATIONS
   ##########################################
-  validates :password presence: true, length: { minimum: 3 }
+  validates :password, presence: true, length: { minimum: 3 }
   validates :password_confirmation, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
