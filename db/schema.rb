64dfa5446959d788030aa8be76aa206a2eb2fb81
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-03-07 00:14:01 -0600

    Remove remember_token from User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130306213131) do
+ActiveRecord::Schema.define(:version => 20130307061156) do
 
   create_table "colleges", :force => true do |t|
     t.string   "officialName"
@@ -53,10 +53,8 @@ ActiveRecord::Schema.define(:version => 20130306213131) do
     t.string   "state"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
-    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
