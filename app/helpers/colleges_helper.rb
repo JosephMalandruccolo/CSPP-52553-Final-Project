a6696c9e0163ec425aeaac3f6bb3dfd41525e519
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-03-13 01:23:18 -0500

    Clean college helpers

--- app/helpers/colleges_helper.rb
+++ app/helpers/colleges_helper.rb
@@ -1,16 +1,2 @@
 module CollegesHelper
-
-
-
-	DEFAULT_SEAL = '/assets/seal/black-chalkboard.jpg'
-	def show_seal(college)
-
-		if @college.logo.nil?
-			image_tag(DEFAULT_SEAL, alt: college.shortName)
-		else
-			image_tag("/assets/seal/#{@college.logo}", alt: college.shortName, size: { height: "50%", width: "50%" })
-		end
-	end
-
-
 end
