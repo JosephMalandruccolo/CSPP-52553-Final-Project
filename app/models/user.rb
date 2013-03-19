c785368e48bd8e98ce3927d91021832afb48848f
Joseph Malandruccolo <jdmalandruccolo@gmail.com>
2013-03-19 18:21:19 -0500

    Add User avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :birthMonth, :birthYear, :birthday, :city, :email, :firstName, :gender, :lastName, :password, :password_confirmation, :state, :admin, :hometown
+  attr_accessible :birthMonth, :birthYear, :birthday, :city, :email, :firstName, :gender, :lastName, :password, :password_confirmation, :state, :admin, :hometown, :avatar_image, :retained_avatar_image, :remove_avatar_image
+
+  image_accessor :avatar_image
 
   has_secure_password
 
@@ -50,6 +52,9 @@ class User < ActiveRecord::Base
   validates :birthYear, presence: true
   validates :city, presence: true
   validates :state, presence: true
+  validates_size_of :avatar_image, maximum: 300.kilobytes
+  validates_property :format, of: :avatar_image, :in => [:jpeg, :png, :gif, :jpg]
+  validates_property :mime_type, of: :avatar_image, :in => ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'], case_sensitive: false
   
 
   ##########################################
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130319094533) do
+ActiveRecord::Schema.define(:version => 20130319224207) do
 
   create_table "cities", :force => true do |t|
     t.string   "city"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20130319094533) do
     t.datetime "updated_at",                                 :null => false
   end
 
+  add_index "cities", ["city"], :name => "index_cities_on_city"
   add_index "cities", ["state"], :name => "index_cities_on_state"
 
   create_table "colleges", :force => true do |t|
@@ -89,9 +90,11 @@ ActiveRecord::Schema.define(:version => 20130319094533) do
     t.integer  "birthYear"
     t.string   "city"
     t.string   "state"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
-    t.boolean  "admin",           :default => false
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
+    t.boolean  "admin",             :default => false
+    t.string   "avatar_image_uid"
+    t.string   "avatar_image_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
